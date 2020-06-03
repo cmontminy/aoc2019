b61d2acdd913ee42ec2ebed8e0d6bc25c6b6a43b
Colette Montminy <colette.montminy@gmail.com>
2020-06-02 23:56:20 -0500

    add cpp stuff

---
+++ day1/day1.cpp
@@ -0,0 +1,19 @@
+// DAY 1: Fuel required to launch a given module is based on its mass. 
+// Specifically, to find the fuel required for a module, take its mass, 
+// divide by three, round down, and subtract 2.
+
+#include <iostream>  
+#include <fstream>
+  
+typedef std::cout cout_std; 
+
+int main() { 
+    
+      
+    return 0; 
+} 
+
+int part_one() {
+
+}
+
